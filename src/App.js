df308e9c2fd4b744155dbc825bb224a5585fff17
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-11 11:35:00 -0600

    styled clock with grid

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import './App.css';
 
 export default class App extends Component {
   render() {
+
+     // return <div className=""><Clock/></div>
+
     return (
         <div className="grid">
             <h1 className="grid__title">Birthday Countdown</h1>
