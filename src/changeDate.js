daff5a2ae154ae4a0ede439ff73194ede4d97ffc
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-12 11:41:41 -0600

    fixed time and got timer object into the clock

--- src/changeDate.js
+++ src/changeDate.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ChangeDate = (title, callBack) => {
     return (
         <button className="change-date" onClick={callBack}>
-            <i class="fas fa-calendar-alt"></i>
+            <i className="fas fa-calendar-alt"></i>
             {title}
         </button>
     )
