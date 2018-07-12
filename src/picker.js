b5f00549ad64a56370fcf438d98d93fdce738860
LindsayWilde <lindsaywilde439@gmail.com>
2018-07-12 10:59:29 -0600

    refactored app and picker js to have dates

--- src/picker.js
+++ src/picker.js
@@ -1,38 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
-import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from "react-datepicker";
+import moment from "moment";
 
 import './App.css';
 
+import 'react-datepicker/dist/react-datepicker.css';
+
 class Picker extends Component {
 
-    constructor (props) {
-        super(props)
-        this.state = {
-            startDate: moment()
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment()
     };
     this.handleChange = this.handleChange.bind(this);
-}
+  }
 
-    handleChange(date) {
-        console.log('trying to change date for', date._d);
-        this.setState({
-            startDate: date
-        });
-    }
+  handleChange(date) {
+    console.log('trying to change date for', date._d);
+    this.setState({
+      startDate: date
+    });
+    this.props.callback(date)
+  }
 
-    render() {
-        return(
-            <div className="picker">
-                <DatePicker
-                    selected={this.state.startDate}
-                    onChange={this.handleChange}
-                />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="picker">
+        <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default Picker;
